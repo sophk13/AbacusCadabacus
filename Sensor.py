4bee229fa912a44be3fa6db1a4246e33d9371e35
JonesyTheTurtle <gage.s.matt@gmail.com>
2025-07-15 16:00:53 -0400

    Sensor.py

--- Sensor.py
+++ Sensor.py
@@ -17,20 +17,20 @@ class Sensor:
         self.I2C = machine.I2C(channel,scl = Pin(scl_pin),sda = Pin(sda_pin))
 
         # D1-D7 are inputs, D0 is output
-        self.I2C.writeto(self.DEV_ADDR,bytearray([CONF_PORT0,0b11111110]))
-        self.I2C.writeto(self.DEV_ADDR,bytearray([CONF_PORT1,0b11111111]))     
+        self.I2C.writeto(self.DEV_ADDR,bytearray([CONF_PORT0,0b11111111]))
+        self.I2C.writeto(self.DEV_ADDR,bytearray([CONF_PORT1,0b01111111]))     
 
         # set polarity of inputs; D1-D7 flip polarity, D0 retain original polarity
-        self.I2C.writeto(self.DEV_ADDR,bytearray([POLARITY_PORT0,0b11111110]))
+        self.I2C.writeto(self.DEV_ADDR,bytearray([POLARITY_PORT0,0b11111111]))
         self.I2C.writeto(self.DEV_ADDR,bytearray([POLARITY_PORT1,0b00000011]))
 
         # Set D0 off (LEDs off)
-        self.I2C.writeto(self.DEV_ADDR,bytearray([OUTPUT_PORT0,0b00000000]))
+        self.I2C.writeto(self.DEV_ADDR,bytearray([OUTPUT_PORT1,0b00000000]))
         
     def read(self):
 
         # Turn on the LEDs in the line sensor
-        self.I2C.writeto(self.DEV_ADDR,bytearray([OUTPUT_PORT0,0b00000001]))
+        self.I2C.writeto(self.DEV_ADDR,bytearray([OUTPUT_PORT1,0b10000000]))
         # allow time for sensors to turn on
         time.sleep_ms(20)
         # Read one byte of data from the I/O expander, this data is D0 = LED control pin; D1-D7 = reflectance sensor signals
@@ -40,10 +40,13 @@ class Sensor:
         byte1 = self.I2C.readfrom(self.DEV_ADDR,1)[0]
 
         # turn off the LEDs in the line sensor
-        self.I2C.writeto(self.DEV_ADDR,bytearray([OUTPUT_PORT0,0b00000000]))
+        self.I2C.writeto(self.DEV_ADDR,bytearray([OUTPUT_PORT1,0b00000000]))
         time.sleep_ms(20)
 
         #needs to be updated to combine both bites into one
+        byte1 = ((byte1<<6)>>6) | (byte1<<7)
+        
+
         return byte0>>1     
 
 if __name__ == "__main__":
